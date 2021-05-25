121588a5c3475fa2ad5918cc6be45c6a7acea183
Reid <rlj0713@gmail.com>
2021-05-25 16:23:04 -0400

    commit tree 5e318ea69363efb77d6c8560d3ffd961872db9a7

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,9 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(fiction non-fiction) }
+
+    
 end
